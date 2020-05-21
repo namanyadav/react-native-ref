1e866882050b7829beb09b6f7d813cf79ed6bafd
naman_yadav <namanyadav@gmail.com>
2020-05-21 14:50:06 -0400

    Text input and button aligned horizontally and with 80% width

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.flexbox}>
+        <TextInput placeholder="Course Goal" style={styles.textinput}/>
+        <Button title="Add"/>
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+      padding: 50
   },
+    flexbox : {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+    textinput : {
+      borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        width: '80%'
+    }
 });
